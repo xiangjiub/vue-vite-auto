0876c6c37bb7038b9c1426e2b3d07daa7f0b3aad
xiangjiub <47174076+xiangjiub@users.noreply.github.com>
2021-01-21 11:15:02 +0800

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="text-xl mt-6">
+    <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
+      <carbon-campsite />
+    </router-link>
+
+    <a class="icon-btn mx-2" :title="t('button.toggle_dark')" @click="isDark = !isDark">
+      <carbon-moon v-if="isDark" />
+      <carbon-sun v-else />
+    </a>
+
+    <a class="icon-btn mx-2" :title="t('button.toggle_langs')" @click="toggleLocales">
+      <carbon-language />
+    </a>
+
+    <router-link class="icon-btn mx-2" to="/about" :title="t('button.about')">
+      <carbon-dicom-overlay />
+    </router-link>
+
+    <a class="icon-btn mx-2" rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank" title="GitHub">
+      <carbon-logo-github />
+    </a>
+  </nav>
+</template>
+
+<script setup lang='ts'>
+import { useI18n } from 'vue-i18n'
+import { isDark } from '/~/logics'
+
+const i18n = useI18n()
+
+const toggleLocales = () => {
+  // change to some real logic
+  const locales = i18n.availableLocales
+  i18n.locale.value = locales[(locales.indexOf(i18n.locale.value) + 1) % locales.length]
+}
+
+const t = i18n.t
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <p class="text-4xl">
+      <carbon-campsite class="inline-block" />
+    </p>
+    <p>
+      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
+        Vitesse
+      </a>
+    </p>
+    <p>
+      <em class="text-sm opacity-75">{{ t('intro.desc') }}</em>
+    </p>
+
+    <div class="py-4" />
+
+    <input
+      id="input"
+      v-model="name"
+      :placeholder="t('intro.whats-your-name')"
+      type="text"
+      autocomplete="false"
+      class="px-4 py-2 border border-gray-200 rounded text-center text-sm outline-none active:outline-none bg-transparent dark:border-gray-700"
+      style="width: 250px"
+      @keydown.enter="go"
+    >
+    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
+
+    <div>
+      <button
+        class="btn m-3 text-sm"
+        :disabled="!name"
+        @click="go"
+      >
+        {{ t('button.go') }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang='ts'>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useI18n } from 'vue-i18n'
+
+const name = ref('')
+
+const router = useRouter()
+const go = () => {
+  if (name.value)
+    router.push(`/hi/${name.value}`)
+}
+
+const { t } = useI18n()
+</script>
